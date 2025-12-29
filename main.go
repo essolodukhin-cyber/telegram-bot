9f0db2e11885f64acb9b6d4a175f8f6316578fb0
Solodukhin Evgeniy Sergeevich <7i06x0ogbyr8xv4cqabm9641l35gy0@bots.bitbucket.org>
2025-12-29 21:14:05 +0300

    test

--- main.go
+++ main.go
@@ -11,15 +11,21 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
+var (
+	envBotToken = os.Getenv("BOTTOKEN")
+	envChatID   = os.Getenv("CHATID")
+	cronTime    = "* * * * *" //"0 12 * * 3" // Каждый среду (3) в 12:00
+)
+
 func main() {
 	// Токен бота
-	botToken := os.Getenv("BOTTOKEN")
-	//botToken := "8437986859:AAHRZcu3gc3kn5cHAoV1Xn2WtHrylxIT9p4" // Например: "8206421086:AAHysZGoynV-vDc6hepIfV1zLp6UvNh9t3I"
-	chatID, err := strconv.ParseInt(os.Getenv("CHATID"), 10, 0)
+	botToken := envBotToken
+	//botToken := "8437986859:AAHRZcu3gc3kn5cHAoV1Xn2WtHrylxIT9p4"
+	chatID, err := strconv.ParseInt(envChatID, 10, 0)
 	if err != nil {
 		log.Fatal(err)
 	}
-	//chatID := int64(-1002880878621) // ID чата (число, например, -925488365)
+	//chatID := int64(-1002880878621)
 
 	// Создаем экземпляр бота
 	bot, err := tgbotapi.NewBotAPI(botToken)
@@ -39,7 +45,7 @@ func main() {
 	c := cron.New()
 
 	// Добавляем задачу (каждую неделю в среду в 12:00)
-	_, err = c.AddFunc("0 12 * * 3", func() { // Каждый среду (3) в 12:00
+	_, err = c.AddFunc(cronTime, func() {
 		msg := tgbotapi.NewMessage(chatID, "Ало!Пора бронить будку!")
 		if _, err := bot.Send(msg); err != nil {
 			log.Println("Ошибка отправки сообщения:", err)
