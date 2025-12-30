e74636ddbd32351b7abb313348cd29eacbd9914f
Solodukhin Evgeniy Sergeevich <7i06x0ogbyr8xv4cqabm9641l35gy0@bots.bitbucket.org>
2025-12-30 19:45:44 +0300

    test 7

--- main.go
+++ main.go
@@ -1,21 +1,13 @@
 package main
 
-import (
-	"log"
-	"strconv"
-
-	"telegram_bot/constants"
-	"telegram_bot/external_services/telegram_bot"
-)
-
 func main() {
-	var bot telegram_bot.TgBot
-	botToken := constants.EnvBotToken
-	chatID, err := strconv.ParseInt(constants.EnvChatID, 10, 0)
-	if err != nil {
-		log.Fatalf("Не смогли получить переменную окружения ChatID. Ошибка %v", err)
-	}
-
-	bot.GetTgBot(botToken)
-	bot.SendMsg(chatID, "Привет очкошники!")
+	//var bot telegram_bot.TgBot
+	//botToken := constants.EnvBotToken
+	//chatID, err := strconv.ParseInt(constants.EnvChatID, 10, 0)
+	//if err != nil {
+	//	log.Fatalf("Не смогли получить переменную окружения ChatID. Ошибка %v", err)
+	//}
+	//
+	//bot.GetTgBot(botToken)
+	//bot.SendMsg(chatID, "Привет очкошники!")
 }
